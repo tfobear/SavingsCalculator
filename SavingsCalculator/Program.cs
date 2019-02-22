7997a30f67681831896060ee28ff902563bbf31c
Thomas Fobear <tfobear@hotmail.com>
2019-02-22 01:30:17 -0500

    initial solution and packages (#1)
    * initial solution and packages

---
+++ SavingsCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SavingsCalculator.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupAppConfig)
+                .UseStartup<Startup>();
+
+        private static void SetupAppConfig(WebHostBuilderContext context, IConfigurationBuilder builder)
+        {
+            // removes defaults
+            builder.Sources.Clear();
+
+            builder.AddJsonFile("./config.json", false, true)
+                .AddEnvironmentVariables();
+        }
+    }
+}
