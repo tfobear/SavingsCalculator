407d832b7d8142e978d165588acae0628ea542a4
Thomas Fobear <tfobear@hotmail.com>
2019-03-03 23:22:01 -0500

    added register feature (#13)

--- SavingsCalculator/Controllers/AccountsController.cs
+++ SavingsCalculator/Controllers/AccountsController.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using SavingsCalculator.Api.Data.ViewModels;
 using SavingsCalculator.Data.Entities;
 using SavingsCalculator.Data.ViewModels;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SavingsCalculator.Controllers
 {
@@ -40,50 +42,45 @@ namespace SavingsCalculator.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await userManager.FindByEmailAsync(login.Email);
-
-                if (user != null)
-                {
-                    var result = await signInManager.CheckPasswordSignInAsync(
-                        user, login.Password, false);
-
-                    if (result.Succeeded)
-                    {
-                        var claims = new[]
-                        {
-                            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
-                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-                        };
-
-                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Tokens:Key"]));
-                        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-                        var token = new JwtSecurityToken(
-                            config["Tokens:Issuer"],
-                            config["Tokens:Audience"],
-                            claims,
-                            expires: DateTime.UtcNow.AddMinutes(30),
-                            signingCredentials: creds
-                        );
-
-                        var results = new
-                        {
-                            token = new JwtSecurityTokenHandler().WriteToken(token),
-                            expiresIn = token.ValidTo
-                        };
-
-                        return Created("", results);
-                    }
-                }
+                return await Login(login.Email, login.Password);
             }
 
             return BadRequest();
         }
 
         [HttpPost]
-        public Task Register()
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel register)
         {
-            throw new InvalidOperationException();
+            var found = await userManager.FindByEmailAsync(register.Email);
+
+            if (found == null)
+            {
+                if (register.Password == register.ConfirmPassword)
+                {
+                    var newUser = new AppUser();
+
+                    newUser.FirstName = register.FirstName;
+                    newUser.LastName = register.LastName;
+                    newUser.Email = register.Email;
+                    newUser.UserName = register.Email;
+
+                    var result = await userManager.CreateAsync(newUser, register.Password);
+
+                    if (result.Succeeded)
+                    {
+                        return await Login(register.Email, register.Password);
+                    }
+                    else
+                    {
+                        var error = result.Errors.First();
+                        return BadRequest(new { message = error.Description, code = error.Code });
+                    }
+                }
+
+                return BadRequest(new { message = "Passwords do not match" });
+            }
+
+            return BadRequest(new { message = "User already exists with email" });
         }
 
         [HttpGet]
@@ -92,5 +89,47 @@ namespace SavingsCalculator.Controllers
         {
             return true;
         }
+
+        private async Task<IActionResult> Login(string email, string password)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+
+            if (user != null)
+            {
+                var result = await signInManager.CheckPasswordSignInAsync(
+                    user, password, false);
+
+                if (result.Succeeded)
+                {
+                    var claims = new[]
+                    {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Tokens:Key"]));
+                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        config["Tokens:Issuer"],
+                        config["Tokens:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddMinutes(30),
+                        signingCredentials: creds
+                    );
+
+                    var results = new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiresIn = token.ValidTo
+                    };
+
+                    return Created("", results);
+                }
+            }
+
+            return BadRequest();
+        }
+
     }
 }
